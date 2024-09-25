c423bd77d2611a412a0ace05696eda9ec4a46417
thiagomag <thiagomagdalena@gmail.com>
2024-09-25 10:14:31 -0300

    changing application name

--- src/main/java/br/com/thiagomagdalena/pacientesservico/RegisterServiceApplication.java
+++ src/main/java/br/com/thiagomagdalena/pacientesservico/RegisterServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PacientesServicoApplication {
+public class RegisterServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PacientesServicoApplication.class, args);
+        SpringApplication.run(RegisterServiceApplication.class, args);
     }
 
 }
